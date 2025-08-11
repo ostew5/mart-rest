6cc06f891b27358cef7980678f3c535311d7e88f
ostew5 <os.stewart.05@gmail.com>
2025-08-11 16:36:51 +1000

    - device map

--- phi3-backend.py
+++ phi3-backend.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 torch.random.manual_seed(0)
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-4k-instruct",
-    device_map="auto",
+    device_map=None,
     torch_dtype="auto",
     trust_remote_code=True,
     load_in_4bit=True,
